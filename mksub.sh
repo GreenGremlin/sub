324e2a334edabcc5e8f046627ac9186c8e8383f4
Jonathan Felchlin <jonathan@energsavvy.com>
2016-11-29 00:44:50 -0800

    Update mksub to take the sub name as the first argument

--- mksub.sh
+++ mksub.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-SUB_COMMAND_PATH=${1%/}
-shift
+
+SUB_NAME=$1
+# SUB_COMMAND_PATH=${2%/}
+SUB_COMMAND_PATH=$(cd $2; pwd)
+
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cat << EOM
 #!/usr/bin/env bash
 set -e
 
-$BASE_DIR/sub.sh \$(basename "\$0") $SUB_COMMAND_PATH "\$@"
+$BASE_DIR/sub.sh $SUB_NAME $SUB_COMMAND_PATH "\$@"
 
 EOM
