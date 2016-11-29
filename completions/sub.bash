bac288ea2257d563ea6dcdbc6bca30f500bbb5a7
Jonathan Felchlin <jonathan@energsavvy.com>
2016-11-29 02:12:09 -0800

    Moving some stuff around

--- completions/sub.bash
+++ completions/sub.bash
@@ -1,24 +1,23 @@
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+_SUB=$SUB_COMMAND_NAME
+_PATH=$SUB_COMMAND_PATH
+
 _sub_completion() {
+  export SUB_COMMAND_NAME=$_SUB
+  export SUB_COMMAND_PATH=$_PATH
+
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
   local prev_word="${COMP_WORDS[COMP_CWORD - 1]}"
-  [ -n "$1" ] && cmd_prefix="$1" || cmd_prefix="$SUB_COMMAND_PREFIX"
-
-  if [[ "$cmd_prefix" = "$SUB_ALIAS" ]]; then
-      cmd_prefix=$SUB_COMMAND_PREFIX
-  fi
 
   if [ "$COMP_CWORD" -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "$(SUB_COMMAND_PREFIX="$cmd_prefix" sub_command_main commands --no-color)" -- "$word") )
+    COMPREPLY=( $(compgen -W "$(sub_command_main commands --no-color)" -- "$word") )
   else
     local command="${COMP_WORDS[1]}"
-    local completions="$(SUB_COMMAND_PREFIX="$cmd_prefix" COMP_PREV="$prev_word" COMP_CWORD="$COMP_CWORD" sub_command_main completions "$command")"
+    local completions="$(COMP_PREV="$prev_word" COMP_CWORD="$COMP_CWORD" sub_command_main completions "$command")"
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
   fi
 }
 
-complete -F _sub_completion $SUB_COMMAND_PREFIX
-
-if [ -n "$SUB_ALIAS" ]; then
-    complete -F _sub_completion $SUB_ALIAS
-fi
+complete -F _sub_completion $SUB_COMMAND_NAME
--- sub.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# parent directory of this script
-BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-
-. "$BASE_DIR/lib/sub.bash"
-
-sub_command_main "$@"
